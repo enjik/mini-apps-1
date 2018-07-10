6d7b278a77ac258d9e77698478579b40020bcd0f
Enji <enjikim17@gmail.com>
2018-07-10 13:51:28 -0700

    server running and html rendering, submit button that sends post request

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,37 @@
+$(document).ready(function() {
+  $('#form').submit(function(event) {
+    $.ajax({
+    method: "POST",
+    url: "http://localhost:3000",
+    dataType: "json",
+    success: function(response) {
+            console.log(response);
+          },
+        error: function(response) {
+            console.log(response);
+          }
+        });
 
+    event.preventDefault();
+  });
+});
 
+// $.ajax({
+//         type: "POST",
+//         url: hb_base_url + "consumer",
+//         contentType: "application/json",
+//         dataType: "json",
+//         data: JSON.stringify({
+//             first_name: $("#namec").val(),
+//             last_name: $("#surnamec").val(),
+//             email: $("#emailc").val(),
+//             mobile: $("#numberc").val(),
+//             password: $("#passwordc").val()
+//         }),
+//         success: function(response) {
+//             console.log(response);
+//         },
+//         error: function(response) {
+//             console.log(response);
+//         }
+// });
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const path = require('path');
+const axios = require('axios');
+//logging middleware
+app.use(function(req, res, next) {
+  console.log(req.method, req.path);
+  next();
+});
+app.use(express.static('client'));
 
+app.listen(3000, () => console.log('Web server listening on localhost:3000 '));
 
+// app.get('/', function());
+//
+app.post('/', function(req, res) {
+  console.log('express post!')
+  res.send('POST request to homepage');
+});
